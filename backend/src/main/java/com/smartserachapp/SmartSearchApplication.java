c611b541913f0b79e8f6d0ebabd07bf4fc291fcf
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2024-10-23 20:00:50 -0400

    Backend Spring App setup

---
+++ backend/src/main/java/com/smartserachapp/SmartSearchApplication.java
@@ -0,0 +1,13 @@
+package com.smartserachapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartserachapp/SmartSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartserachapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
