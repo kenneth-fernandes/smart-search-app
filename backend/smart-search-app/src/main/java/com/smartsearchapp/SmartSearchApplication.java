8e4195fac9ab1a37583d22979577dddefe8fdffe
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2024-10-23 21:05:24 -0400

    Updated backend files

--- backend/smart-search-app/src/main/java/com/smartsearchapp/SmartSearchApplication.java
+++ backend/smart-search-app/src/main/java/com/smartsearchapp/SmartSearchApplication.java
@@ -1,4 +1,4 @@
-package com.smartserachapp;
+package com.smartsearchapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/smart-search-app/src/test/java/com/smartsearchapp/SmartSearchApplicationTests.java
+++ backend/smart-search-app/src/test/java/com/smartsearchapp/SmartSearchApplicationTests.java
@@ -1,4 +1,4 @@
-package com.smartserachapp;
+package com.smartsearchapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
